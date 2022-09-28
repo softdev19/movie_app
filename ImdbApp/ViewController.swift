ed0b4b6c338a9bd72fe312033d16412143b85441
collinrentz7 <collinrentz2@gmail.com>
2022-09-28 19:57:21 +0300

    Initial commit

---
+++ ImdbApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ImdbApp
+//
+//  Created by Ivan Lyaskovets on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
