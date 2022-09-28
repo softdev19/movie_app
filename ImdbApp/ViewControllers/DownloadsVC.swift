1e256ff05a1b60a1c086286a447d211d166c563b
collinrentz7 <collinrentz2@gmail.com>
2022-09-28 20:20:40 +0300

    Add: start UI
    Added 3 viewControllers - HomeVC, SearchVC, DownloadsVC
    ViewControllers were connected in TabBarController with titles and images

---
+++ ImdbApp/ViewControllers/DownloadsVC.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadsVC.swift
+//  ImdbApp
+//
+//  Created by Ivan Lyaskovets on 28.09.2022.
+//
+
+import UIKit
+
+class DownloadsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView(){
+        view.backgroundColor = .systemRed
+    }
+
+}
