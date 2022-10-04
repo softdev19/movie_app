1753a8543da2e3a31ea0bf0cdaa623ce83adb59e
collinrentz7 <collinrentz2@gmail.com>
2022-10-04 16:29:43 +0300

    Add: Simple UI for FavouritesVC

--- ImdbApp/MVC/ViewControllers/Body/Menu/Favourites/FavouritesVC.swift
+++ ImdbApp/MVC/ViewControllers/Body/Menu/Favourites/FavouritesVC.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
+import SnapKit
 
 class FavouritesVC: UIViewController {
+    
+    lazy private var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .plain)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "favouriteCell")
+        return table
+    }()
+    
+    private var videos: [Video] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +24,45 @@ class FavouritesVC: UIViewController {
     }
 
     private func setupView(){
+        
+        title = "Favourites"
+        navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(tableView)
+        setupTableView()
     }
+    
+    private func setupTableView(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+        }
+    }
+}
 
+extension FavouritesVC: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favouriteCell", for: indexPath)
+        return cell
+    }
+}
+
+extension FavouritesVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
