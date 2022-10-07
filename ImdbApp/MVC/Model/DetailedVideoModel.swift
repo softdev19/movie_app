e86ab7d57c3a07e2c2a3c631237a4e15ffa16433
collinrentz7 <collinrentz2@gmail.com>
2022-10-07 18:28:50 +0300

    Create DetailedVideoModel

---
+++ ImdbApp/MVC/Model/DetailedVideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  DetailedVideoModel.swift
+//  ImdbApp
+//
+//  Created by Ivan Lyaskovets on 07.10.2022.
+//
+
+import Foundation
+
+struct DetailedVideoModel{
+    let title: String?
+    let year: String?
+    let video: String?
+}
